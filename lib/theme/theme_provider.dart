e362357b1f247fbb73bd02048a203e794b0651e1
rahulgope45 <rahuleile456@gmail.com>
2025-10-04 23:24:01 +0530

    theme Provider buit

---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dark_mode.dart';
+import 'light_mode.dart';
+
+
+class ThemeProvider  extends ChangeNotifier{
+
+     //Intially Light Mode
+     ThemeData _themeData = lightMode;
+
+     //Getting theme data
+     ThemeData get themedata => _themeData;
+
+     //Is current theme is dark mode
+     bool get isDarkMode => _themeData == darkMode;
+
+     //set theme
+     set  themedata(ThemeData themedata){
+      _themeData = themedata;
+      notifyListeners();
+     }
+
+     //toggle theme
+     void toggleTheme(){
+      if (_themeData == lightMode){
+        themedata = darkMode;
+      }
+      else{
+       themedata = lightMode;
+      }
+     }
+
+
+}

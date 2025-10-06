045e2db403912c6cf6e389d4822d0505b8f63f22
rahulgope45 <rahuleile456@gmail.com>
2025-10-06 18:33:35 +0530

    Genrated collection files

---
+++ lib/models/app_settings.dart
@@ -0,0 +1,11 @@
+import 'package:isar/isar.dart';
+
+//run cmd to genrate file: dart run build_runner build
+part 'app_settings.g.dart';
+
+@Collection()
+class AppSettings {
+  Id id = Isar.autoIncrement;
+  DateTime? firsLaunchDate;
+
+}
--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -1,3 +1,22 @@
+import 'package:isar/isar.dart';
 
+//run cmd to genrate file: dart run build_runner build
+part 'habit.g.dart';
+
+@Collection()
+class Habit { 
+
+  //habbit id
+  Id id = Isar.autoIncrement;
+
+  //habbit Name
+  late String name;
+
+  //Completed Days
+  //DateTime{year,month, day},
+  //DateTime{2025,1, 1},
+  //DateTime{2025,1, 2},
+
+}
 
 

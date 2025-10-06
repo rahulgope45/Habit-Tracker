d118339f91cd56787026620607afb77b88e8fc53
rahulgope45 <rahuleile456@gmail.com>
2025-10-06 23:41:59 +0530

    Database create a new habbit added and save to db

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:habit_tracker/models/app_settings.dart';
 import 'package:habit_tracker/models/habit.dart';
 import 'package:isar/isar.dart';
@@ -35,6 +36,38 @@ class HabitDatabase extends ChangeNotifier{
       final settings = await isar.appSettings.where().findFirst();
       return settings?.firstLaunchDate;
    }
+
+
+   //Crud operations
+
+
+   //List of habits
+   final List<Habit> currentHabits = [];
+
+   //Create add a new habbit 
+   Future<void> addHabits(String habitName) async {
+      //create a new habit
+      final newHabit = Habit()..name = habitName;
+
+      //save to db
+      await isar.writeTxn(() => isar.habits.put(newHabit));
+
+      //re-read from db
+      readHabits();
+
+
+
+   }
+
+   //READ -read saved habits from db
+   Future<void> readHabits() async {
+      //fetch all habits
+      List<Habit> fetchedHabits = await isar.habits.where().findAll();
+
+      
+   }
+
+
 
 
 
